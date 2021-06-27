9d0c660873eb04b91f4aec5ddb50368aadd0b5fb
Zmaplex <zmaplex@gmail.com>
2021-06-27 23:13:53 +0800

    增加搜索字段

--- public/admin.py
+++ public/admin.py
@@ -8,4 +8,4 @@ from public.models import IPInfo
 class IPInfoAdmin(admin.ModelAdmin):
     list_display = ['ipaddress', 'asn_info', 'recaptcha_score', 'risk', 'update_at', 'source_ip']
     list_filter = ('risk',)
-    search_fields = ['ipaddress']
+    search_fields = ['ipaddress', 'source_ip', 'asn_info']
